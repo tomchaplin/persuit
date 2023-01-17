94841332ddc56b3af4d7b093ae4290b7cf7564fe
Thomas Chaplin <tomrchaplin@gmail.com>
2023-01-17 12:32:39 +0000

    First working implementation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # persuit
+
+## TODO
+
+- [ ] Try out FnvHasMap, IndexMap and BTreeMap for storing low_inverse
+- [ ] Try out linked lists and bitvecs as Column types
+- [ ] Return representatives
+- [ ] Python bindings
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,42 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::iter::Enumerate;
+use std::marker::Send;
+use std::{collections::HashMap, thread};
 
-trait Column {
+pub trait Column: Send {
     fn pivot(&self) -> Option<usize>;
     fn add_col(&mut self, other: &Self);
 }
 
+#[derive(Default)]
 struct VecColumn {
     col: Vec<usize>,
+}
+
+impl VecColumn {
+    // Returns the index where we should try to insert next entry
+    fn add_entry(&mut self, entry: usize, starting_idx: usize) -> usize {
+        let mut working_idx = starting_idx;
+        while let Some(value_at_idx) = self.col.iter().nth(working_idx) {
+            match value_at_idx.cmp(&entry) {
+                Ordering::Less => {
+                    working_idx += 1;
+                    continue;
+                }
+                Ordering::Equal => {
+                    self.col.remove(working_idx);
+                    return working_idx;
+                }
+                Ordering::Greater => {
+                    self.col.insert(working_idx, entry);
+                    return working_idx + 1;
+                }
+            }
+        }
+        // Bigger than all idxs in col - add to end
+        self.col.push(entry);
+        return self.col.len() - 1;
+    }
 }
 
 impl Column for VecColumn {
@@ -15,11 +45,17 @@ impl Column for VecColumn {
     }
 
     fn add_col(&mut self, other: &Self) {
-        todo!()
+        let mut working_idx = 0;
+        for entry in other.col.iter() {
+            working_idx = self.add_entry(*entry, working_idx);
+        }
     }
 }
 
-struct StandardAlgo<C: Column, T: Iterator<Item = (usize, C)>> {
+type Pairing = (usize, usize);
+
+#[derive(Default)]
+pub struct StandardAlgo<C: Column, T: Iterator<Item = (usize, C)>> {
     enumerated_cols: T,
     // Takes in a low and returns the column with that low
     low_inverse: HashMap<usize, C>,
@@ -40,6 +76,7 @@ impl<C: Column, T: Iterator<Item = (usize, C)>> StandardAlgo<C, T> {
     }
 }
 
+// A StandardAlgo struct can be viewed as an iterator over pairings
 impl<C: Column, T: Iterator<Item = (usize, C)>> Iterator for StandardAlgo<C, T> {
     type Item = Pairing;
 
@@ -62,19 +99,58 @@ impl<C: Column, T: Iterator<Item = (usize, C)>> Iterator for StandardAlgo<C, T> 
     }
 }
 
-type Pairing = (usize, usize);
+pub fn persuit<C, T>(col_iterator: T) -> StandardAlgo<C, flume::IntoIter<(usize, C)>>
+where
+    T: Iterator<Item = C> + Send + 'static,
+    C: Column + 'static,
+{
+    let (tx, rx) = flume::unbounded();
+    thread::spawn(move || {
+        let mut enumerated_cols = col_iterator.enumerate();
+        while let Some(enum_col) = enumerated_cols.next() {
+            tx.send(enum_col).unwrap();
+        }
+    });
+    StandardAlgo {
+        enumerated_cols: rx.into_iter(),
+        low_inverse: HashMap::new(),
+    }
+}
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn persuit_serial<C, T>(col_iterator: T) -> StandardAlgo<C, Enumerate<T>>
+where
+    T: Iterator<Item = C> + Send + 'static,
+    C: Column + 'static,
+{
+    StandardAlgo {
+        enumerated_cols: col_iterator.enumerate(),
+        low_inverse: HashMap::new(),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let file = File::open("test.mat").unwrap();
+        let columns: Vec<VecColumn> = BufReader::new(file)
+            .lines()
+            .map(|l| {
+                let l = l.unwrap();
+                if l.is_empty() {
+                    vec![]
+                } else {
+                    l.split(",").map(|c| c.parse().unwrap()).collect()
+                }
+            })
+            .map(|l| VecColumn { col: l })
+            .collect();
+        let pairings: Vec<Pairing> = persuit(columns.into_iter()).collect();
+        let correct = vec![(1, 4), (2, 5), (3, 6), (8, 9), (7, 10)];
+        assert_eq!(pairings, correct)
     }
 }
